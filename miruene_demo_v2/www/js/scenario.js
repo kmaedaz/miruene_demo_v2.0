ef92f610873d06b802bb315c2d2927065018bf34
admin <admin@DESKTOP-UP0MOD9>
2017-01-26 21:59:34 +0900

    シナリオ途中

--- miruene_demo_v2/www/js/scenario.js
+++ miruene_demo_v2/www/js/scenario.js
@@ -1,7 +1,66 @@
 // This is a JavaScript file
 
 // シナリオ実行
-function scenario_play(scenario_arr){
+
+var scenario_data = {
+    "SCENARIO_HEATPROTECT": {
+        "NAME": "熱中症予防",
+        "PARAM": "1:HIDE:#scene_top_button,2:IR_COMMAND:THERMOMETER_UP,2:IR_COMMAND:THERMOMETER_UP1,3:WAIT:4,5:IR_COMMAND:CURTAIN_CLOSE,6:WAIT:3,7:IR_COMMAND:AIRCON_ON, 8:SHOW:#scene_top_button"
+    },
+    "SCENARIO_CRIMEDEFEND": {
+        "NAME": "防犯",
+        "PARAM": "0:HIDE:#scene_top_button,1:IR_COMMAND:STAGING_LIGHT_ON, 2:WAIT:4, 3:IR_COMMAND:LIGHT_ON, 4:WAIT:2, 7:IR_COMMAND:CURTAIN_CLOSE, 8:SHOW:#scene_top_button"
+    },
+
+    "SCENARIO_WAKEUP": {
+        "NAME": "起床",
+        "PARAM": "0:HIDE:#scene_top_button,1:IR_COMMAND:CURTAIN_CLOSE,2:WAIT:4,3:IR_COMMAND:AUDIO_PLAY,4:WAIT:2, 5:IR_COMMAND:CURTAIN_OPEN, 6:SHOW:#scene_top_button"
+    },
+
+    "SCENARIO_OUTING": {
+        "NAME": "外出",
+        "PARAM": "0:HIDE:#scene_top_button,2:WAIT:1,3:IR_COMMAND:LIGHT_ON,4:WAIT:1, 3:IR_COMMAND:AIRCON_ON,4:WAIT:5, 6:IR_COMMAND:AIRCON_OFF,7:WAIT:3,8:IR_COMMAND:LIGHT_OFF,12:SHOW:#scene_top_button"
+    },
+
+    "SCENARIO_RETURNING": {
+        "NAME": "帰宅",
+        "PARAM": "1:HIDE:#scene_top_button,1:WAIT:1,4:IR_COMMAND:LIGHT_ON, 3:WAIT:3, 5:IR_COMMAND:AIRCON_ON, 3:WAIT:9,6:SHOW:#scene_top_button"
+    },
+
+
+    "SCENARIO_POST_HEATPROTECT": {
+        "NAME": "熱中症予防後処理",
+        "PARAM": "1:IR_COMMAND:THERMOMETER_DOWN,1:WAIT:1,2:IR_COMMAND:CURTAIN_OPEN,1:WAIT:2,3:IR_COMMAND:AIRCON_OFF,4:IR_COMMAND:AIRCON_ACONOFF"
+    },
+
+    "SCENARIO_POST_CRIMEDEFEND": {
+        "NAME": "防犯後処理",
+        "PARAM": "1:IR_COMMAND:STAGING_LIGHT_OFF, 2:IR_COMMAND:LIGHT_OFF,1:WAIT:1,3:IR_COMMAND:CURTAIN_OPEN"
+    },
+
+    "SCENARIO_POST_WAKEUP": {
+        "NAME": "起床後処理",
+        "PARAM": "1:IR_COMMAND:AUDIO_STOP,2:WAIT:1,3:IR_COMMAND:AUDIO_OFF, 4:IR_COMMAND:CURTAIN_OPEN,5:WAIT:1,6:IR_COMMAND:LIGHT_OFF"
+    },
+
+    "SCENARIO_POST_OUTING": {
+        "NAME": "外出後処理",
+        "PARAM": "1:IR_COMMAND:LIGHT_OFF,2:IR_COMMAND:AIRCON_OFF,3:IR_COMMAND:CURTAIN_OPEN"
+    },
+
+    "SCENARIO_POST_RETURNING": {
+        "NAME": "帰宅後処理", "PARAM": "1:IR_COMMAND:AIRCON_OFF,1:IR_COMMAND:LIGHT_OFF,4:IR_COMMAND:AIRCON_ACONOFF"
+    },
+
+
+    "SCENARIO_ALLT_RESET": {
+        "NAME": "シナリオ全初期化",
+        "PARAM": "0:IR_COMMAND:TV_ACONOFF,1:IR_COMMAND:LAMP_ON, 2:IR_COMMAND:THERMOMETER_DOWN, 3:IR_COMMAND:AIRCON_OFF, 4:IR_COMMAND:PURIFER_OFF, 5:IR_COMMAND:TV_ACONOFF, 6:IR_COMMAND:LIGHT_OFF,7:IR_COMMAND:AUDIO_ACONOFF,7:IR_COMMAND:AUDIO_ACONOFF,8:IR_COMMAND::AIRCON_ACONOFF"
+    },
+};
+
+
+function scenario_play(scenario_arr) {
     console.log(scenario_arr);
     if(!scenario_arr || scenario_arr== undefined){
     	return false;	
@@ -48,7 +107,7 @@ function scenario_play(scenario_arr){
 
         } else if(key=="IR_COMMAND"){
             console.log("COMMAND"+val);
-            sendIRkit_command(val);
+            sendIRUSB_command(val);
             setTimeout(    function (){
                  scenario_play(next_arr);
             } , n*300);
@@ -90,26 +149,20 @@ function scenario_play(scenario_arr){
 
 // シナリオ実行
 function scenario_exec(val){
-    var tmp= localStorage.getItem(val);
+    var tmp = scenario_data;
     var param;
     var param1;
     console.log(tmp);
-    if(!tmp)
-    {
-     return ;    
-    }
-    try {
-        param=  JSON.parse(tmp);
-        } catch (e) {
-        console.error("erro");
-            return false;
-        }
-        console.log(param);
+
+    
+    var param = tmp[val];
+
+    var result1 = param.PARAM.split(",");
+    console.log("-------------------------");
+
+    console.log(result1);
 
 
-	console.log(param.PARAM);
-	var result1 = param.PARAM.split(",");
-	console.log(result1);
     scenario_play(result1);
 	return true;
 
